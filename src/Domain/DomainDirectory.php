35f08199203bff92b15fc1abb4da6f521b42008b
Constantin GALBENU <gica.galbenu@gmail.com>
2017-03-27 21:17:01 +0300

    updated gitignore to add vendor

---
+++ src/Domain/DomainDirectory.php
@@ -0,0 +1,15 @@
+<?php
+/******************************************************************************
+ * Copyright (c) 2017 Constantin Galbenu <gica.galbenu@gmail.com>             *
+ ******************************************************************************/
+
+namespace Crm;
+
+
+class DomainDirectory
+{
+    public static function getDomainDirectory()
+    {
+        return __DIR__;
+    }
+}
---
+++ src/Infrastructure/Cqrs/Directory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 2017 Constantin Galbenu <xprt64@gmail.com>
+ */
+
+namespace Domain;
+
+
+class DomainDirectory
+{
+    public static function getDomainDirectory()
+    {
+        return __DIR__;
+    }
+}
